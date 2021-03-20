d847d167740b552c2153d0f5c84b690eb172f4b5
Shresth-Sharma <76861631+Shresth-Sharma@users.noreply.github.com>
2021-03-20 11:48:40 +0530

    Add files via upload

---
+++ Player.js
@@ -0,0 +1,14 @@
+class Player{
+    constructor(){}
+    getCount(){
+        var playerCountref = database.ref('playerCount')
+        playerCountref.on("value",(data)=>{
+            playerCount=data.val();
+        })
+    }
+    updateCount(count){
+     database.ref('/').update({
+         playerCount:count
+     })
+    }
+}
